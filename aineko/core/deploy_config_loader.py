64086b8c7576988210bc78f13354c72560d8d8f6
Barabazs <31799121+Barabazs@users.noreply.github.com>
2023-09-28 16:54:11 +0200

    feat: remove project from config

--- aineko/core/deploy_config_loader.py
+++ aineko/core/deploy_config_loader.py
@@ -87,7 +87,6 @@ def _generate_full_config(
     if not user_config:
         raise ValueError("User config has not been loaded nor defined.")
     full_config: dict = {
-        "project": user_config.project,
         "version": user_config.version,
         "environments": defaultdict(lambda: {"pipelines": []}),
     }
