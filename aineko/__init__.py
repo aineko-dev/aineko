0c63d75abfd122846fac4c79d4b113d5b887d0e2
craycrayfish <shawntan72@gmail.com>
2023-11-03 16:21:00 -0400

    chore: bump to 0.2.1 (#63)
    * chore: update version to 0.2.1

--- aineko/__init__.py
+++ aineko/__init__.py
@@ -3,7 +3,7 @@
 """Package information for the Aineko package."""
 
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Convex Labs Engineering"
 
 from aineko.core.config_loader import ConfigLoader
