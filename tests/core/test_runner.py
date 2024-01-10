34630b265f832ed007d21666d3346cad3b0514db
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-01-10 21:10:24 +0100

    Fix type hinting in test functions

--- tests/core/test_runner.py
+++ tests/core/test_runner.py
@@ -1,29 +1,31 @@
 # Copyright 2023 Aineko Authors
 # SPDX-License-Identifier: Apache-2.0
 """Tests for the aineko.core.runner module."""
+from typing import Type
+
 from aineko.core.runner import Runner
 
 
-def test_run(runner: type[Runner]) -> None:
+def test_run(runner: Type[Runner]) -> None:
     # Test that ray has run with expected nodes
     # runner.run()
     assert True
 
 
-def test_load_pipeline_config(runner: type[Runner]) -> None:
+def test_load_pipeline_config(runner: Type[Runner]) -> None:
     # Test that ray has run with expected nodes
     # config = runner.load_pipeline_config()
     assert True
 
 
-def test_prepare_datasets(runner: type[Runner]) -> None:
+def test_prepare_datasets(runner: Type[Runner]) -> None:
     # Test that ray has run with expected nodes
     # pipeline_config = runner.load_pipeline_config()
     # runner.prepare_datasets(pipeline_config)
     assert True
 
 
-def test_run_nodes(runner: type[Runner]) -> None:
+def test_run_nodes(runner: Type[Runner]) -> None:
     # Test that ray has run with expected nodes
     # pipeline_config = runner.load_pipeline_config()
     # runner.prepare_datasets(pipeline_config)
