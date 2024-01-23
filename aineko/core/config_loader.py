bc25cdaef3b4f3fef40b56a0dee8c96863ecf491
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-01-23 16:14:18 +0100

    chore: remove dead code (#136)
    * chore: remove unused _update_params
    
    * chore: remove unused 'env_vars' attribute from config_loader.py and deploy_config_schema_internal.py

--- aineko/core/config_loader.py
+++ aineko/core/config_loader.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 """Module to load config files."""
 import logging
-from typing import overload
 
 from pydantic import ValidationError
 
@@ -63,56 +62,3 @@ class ConfigLoader:
             raise e
 
         return config
-
-    @overload
-    def _update_params(self, value: dict, params: dict) -> dict:
-        ...
-
-    @overload
-    def _update_params(self, value: list, params: dict) -> list:
-        ...
-
-    @overload
-    def _update_params(self, value: str, params: dict) -> str:
-        ...
-
-    @overload
-    def _update_params(self, value: int, params: dict) -> int:
-        ...
-
-    def _update_params(
-        self, value: dict | list | str | int, params: dict
-    ) -> dict | list | str | int:
-        """Update value with params.
-
-        Recursively calls the method if value is a list or dictionary until it
-        reaches a string or int. If string then formats the str with variable
-        mapping in params dict.
-
-        Args:
-            value: value to update
-            params: params to update value with
-
-        Returns:
-            object with updated values (dict, list, str, or int)
-        """
-        if isinstance(value, dict):
-            new_dict_val = {}
-            for key, val in value.items():
-                new_dict_val[key] = self._update_params(val, params)
-            return new_dict_val
-        if isinstance(value, list):
-            new_list_val: list = []
-            for val in value:
-                new_list_val.append(self._update_params(val, params))
-            return new_list_val
-        if isinstance(value, str):
-            for key, val in params.items():
-                value = value.replace(f"${key}", val)
-            return value
-        if isinstance(value, (int, float)):
-            return value
-        raise ValueError(
-            f"Invalid value type {type(value)}. "
-            "Expected dict, list, str, or int."
-        )
