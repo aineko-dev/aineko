75bde7811ff8018e38202330927196c2476c70b2
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-02-09 17:54:10 +0100

    feat: replace extras with aineko-plugins - BLD-1343 (#154)
    * feat: remove aineko extras in favor of aineko-plugins
    
    * doc: update docs for aineko-plugins
    
    * chore: add aineko-style back to pylint plugins
    
    * chore: bump aineko to 0.3.2

--- aineko/extras/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2023 Aineko Authors
-# SPDX-License-Identifier: Apache-2.0
-"""Extra submodules for Aineko."""
-from aineko.extras.connectors.http_poller import HTTPPoller
-from aineko.extras.connectors.websocket_client import WebSocketClient
-from aineko.extras.fastapi.main import FastAPI
