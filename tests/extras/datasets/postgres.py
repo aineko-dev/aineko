4c6308195ce76b484fe898dc8da917be3949e6e3
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-02-13 15:29:15 +0100

    tests: update test cases

--- tests/extras/datasets/postgres.py
+++ tests/extras/datasets/postgres.py
@@ -1,10 +1,12 @@
 # Copyright 2023 Aineko Authors
 # SPDX-License-Identifier: Apache-2.0
 """Tests for Postgres dataset."""
+# pylint: disable=redefined-outer-name
+from contextlib import asynccontextmanager
 
 import pytest
 
-from aineko.extras import AWSDatasetHelper, Postgres
+from aineko.extras import AsyncPostgresDataset, AWSDatasetHelper
 
 
 class TestAWSDatasetHelper:
@@ -17,56 +19,82 @@ class TestAWSDatasetHelper:
         assert isinstance(endpoint, str)
 
 
-@pytest.fixture(scope="module")
-def postgres_table() -> Postgres:
-    """Create a postgres table for testing."""
-    dataset = Postgres(
+@pytest.fixture
+def postgres_db() -> AsyncPostgresDataset:
+    """Create an instance of the Postgres dataset with predefined parameters."""
+    dataset = AsyncPostgresDataset(
         name="test_table",
         host="shawn-dataset.ch4nbfnhdik6.us-east-1.rds.amazonaws.com",
         dbname="postgres_db",
         user="postgres",
         password="testpassword",
     )
-    yield dataset
-    # dataset.delete(if_exists=True)
+    return dataset
+
+
+@pytest.fixture
+@asynccontextmanager
+async def pg_session_with_table(postgres_db: AsyncPostgresDataset):
+    """Create a connected instance of the Postgres dataset with a test table.
+
+    This fixture creates a connected instance of the AsyncPostgresDataset with
+    a test table. The table is deleted after the test is run.
+
+    Yields:
+        A connected instance of the AsyncPostgresDataset with a test table.
+
+    """
+    async with postgres_db as dataset:
+        if await dataset.exists() is False:
+            await dataset.create(
+                schema={"id": "SERIAL PRIMARY KEY", "name": "VARCHAR(255)"}
+            )
+            yield dataset
+        else:
+            yield dataset
+        await dataset.delete(if_exists=True)
+
+
+@pytest.fixture
+@asynccontextmanager
+async def pg_session_without_table(postgres_db: AsyncPostgresDataset):
+    """Create a connected instance of the Postgres dataset without a test table.
+
+    This fixture creates a connected instance of the AsyncPostgresDataset and
+    deletes the test table if it exists.
+
+    Yields:
+        A connected instance of the AsyncPostgresDataset without a test table.
+    """
+    async with postgres_db as dataset:
+        await dataset.delete(if_exists=True)
+        yield dataset
 
 
 class TestPostgres:
     """Tests for Postgres dataset."""
 
     @pytest.mark.asyncio
-    async def test_create(self, postgres_table: Postgres):
-        """Test create."""
-        await postgres_table.connect()
-
-        await postgres_table.create(
-            schema={"id": "SERIAL PRIMARY KEY", "name": "VARCHAR(255)"},
-        )
-        assert await postgres_table.exists()
-
-    @pytest.mark.asyncio
-    async def test_delete(self, postgres_table: Postgres):
-        """Test delete."""
-        await postgres_table.connect()
-
-        await postgres_table.delete()
-        assert await postgres_table.exists() is False
+    async def test_create(
+        self, pg_session_without_table, postgres_db: AsyncPostgresDataset
+    ):
+        """Test the create method."""
+        async with pg_session_without_table as dataset:
+            await dataset.create(
+                schema={"id": "SERIAL PRIMARY KEY", "name": "VARCHAR(255)"},
+            )
+            assert await postgres_db.exists()
 
     @pytest.mark.asyncio
-    async def test_exists(self, postgres_table: Postgres):
-        """Test exists."""
-        await postgres_table.connect()
+    async def test_delete(self, pg_session_with_table: AsyncPostgresDataset):
+        """Test the delete method."""
+        async with pg_session_with_table as dataset:
+            assert await dataset.exists() is True
+            await dataset.delete(if_exists=True)
+            assert await dataset.exists() is False
 
-        await postgres_table.create(
-            schema={"id": "SERIAL PRIMARY KEY", "name": "VARCHAR(255)"},
-        )
-        assert await postgres_table.exists() is True
-
-        # assert await postgres_table.exists() is False
-        # await postgres_table.create(
-        #     schema={"id": "SERIAL PRIMARY KEY", "name": "VARCHAR(255)"},
-        # )
-        # assert await postgres_table.exists() is True
-        # await postgres_table.delete()
-        # assert await postgres_table.exists() is False
-        # print("Table created")
+    @pytest.mark.asyncio
+    async def test_exists(self, pg_session_with_table: AsyncPostgresDataset):
+        """Test the exists method."""
+        async with pg_session_with_table as dataset:
+            assert await dataset.exists() is True
