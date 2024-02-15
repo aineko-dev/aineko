3603bfb92fc282b776e7da84ed7a03b5d98ab127
craycrayfish <shawntan72@gmail.com>
2024-02-15 10:02:17 -0500

    feat: cli dream upgrade (#151)
    * feat: dream cli to have create and check commands
    
    * docs: improved docstrings
    
    * feat: added defaults for CLI options
    
    * docs: added CLI docs
    
    * docs: update aineko dream docs
    
    * fix: arg default error
    
    * chore: update lint

--- aineko/cli/dream.py
+++ aineko/cli/dream.py
@@ -2,49 +2,188 @@
 # SPDX-License-Identifier: Apache-2.0
 """A wrapper around API calls to the Aineko Dream API."""
 
+import time
+
 import click
 import requests
 
 
-@click.command()
-@click.argument("prompt")
-@click.argument("api-key")
+def create_project_from_prompt(
+    api_key: str,
+    url: str,
+    prompt: str,
+) -> str:
+    """Create a project from a prompt.
+
+    Args:
+        api_key: API key to use for the Aineko Dream API.
+        url: URL to use for the Aineko Dream API.
+        prompt: Prompt to generate a project from.
+
+    Returns:
+        request_id: Request ID for the project generation.
+
+    Raises:
+        click.ClickException: If there is an error generating the project.
+    """
+    try:
+        r = requests.get(
+            url=f"{url}/create",
+            params={"prompt": prompt},
+            headers={"x-api-key": api_key},
+            timeout=60,
+        )
+        click.echo(f"Received response code: {r.status_code}")
+        if r.status_code == 202:
+            return r.json()["request_id"]
+
+        else:
+            raise click.ClickException(
+                f"received status code {r.status_code} with message: {r.text}"
+            )
+
+    except Exception as e:
+        raise click.ClickException(
+            f"Error generating project at Aineko Dream API at {url}: {e}"
+        )
+
+
+def check_request_status(
+    api_key: str,
+    url: str,
+    request_id: str,
+    timeout: int = 300,
+) -> str:
+    """Check the status of a request.
+
+    Args:
+        api_key: API key to use for the Aineko Dream API.
+        url: URL to use for the Aineko Dream API.
+        request_id: Request ID to check status for.
+        timeout: Seconds to wait for successful project generation
+            before timing out.
+
+    Returns:
+        Status message.
+
+    Raises:
+        click.ClickException: If there is an error checking the request status.
+    """
+    start_time = time.time()
+    while True:
+        time_elapsed = time.time() - start_time
+        if time_elapsed > timeout:
+            raise click.ClickException(
+                "Timed out waiting for project to generate. "
+            )
+
+        try:
+            r = requests.get(
+                url=f"{url}/result",
+                params={"request_id": request_id},
+                headers={"x-api-key": api_key},
+                timeout=60,
+            )
+        except Exception as e:
+            raise click.ClickException(
+                "Error checking request status at Aineko Dream API "
+                f"at {url}: {e}"
+            )
+
+        if r.status_code == 204:
+            click.echo(
+                f"Project generating... (time elapsed: {int(time_elapsed)}s)"
+            )
+            time.sleep(10)
+            continue
+
+        elif r.status_code == 200:
+            return (
+                "Successfully published project to: "
+                f"https://github.com/Convex-Labs/dream-catcher/tree/{request_id}. \n\n"  # pylint: disable=line-too-long
+                "To create a new project from this dream, run: \n\n"
+                f"aineko create --repo Convex-Labs/dream-catcher#{request_id}"
+            )
+
+        else:
+            raise click.ClickException(
+                "Received unexpected status code "
+                f"{r.status_code} with message: {r.text}"
+            )
+
+
+@click.group()
+def dream() -> None:
+    """Aineko Dream CLI."""
+    pass
+
+
+@dream.command()
+@click.argument("request-id")
+@click.option(
+    "-k",
+    "--api-key",
+    default="",
+    help="API key to use for the Aineko Dream API.",
+)
 @click.option(
     "-u",
     "--url",
     default="https://dream.ap-6d356ccd-bfba-4110-994a-fe164ab8bf77.aineko.app",
     help="API url to use for the Aineko Dream API.",
 )
-def dream(prompt: str, api_key: str, url: str) -> None:
-    """Main function to generate an aineko project using Aineko Dream.\f
+@click.option(
+    "-t",
+    "--timeout",
+    default=300,
+    help=(
+        "Seconds to wait for successful project generation before "
+        "timing out. Will poll status every 10 seconds."
+    ),
+)
+def check(request_id: str, api_key: str, url: str, timeout: int) -> None:
+    """Check the status of a project generation request.
+
+    Args:
+        request_id: Request ID to check status for.
+        api_key: API key to use for the Aineko Dream API.
+        url: URL to use for the Aineko Dream API.
+        timeout: Seconds to wait for successful project
+            generation before timing out.
+    """
+    status = check_request_status(api_key, url, request_id, timeout)
+    click.echo(status)
+
+
+@dream.command()
+@click.argument("prompt")
+@click.option(
+    "-k",
+    "--api-key",
+    default="",
+    help="API key to use for the Aineko Dream API.",
+)
+@click.option(
+    "-u",
+    "--url",
+    default="https://dream.ap-6d356ccd-bfba-4110-994a-fe164ab8bf77.aineko.app",
+    help="API url to use for the Aineko Dream API.",
+)
+def create(
+    prompt: str,
+    api_key: str,
+    url: str,
+) -> None:
+    """Command to generate an aineko project using Aineko Dream.\f
 
     Args:
         prompt: Prompt to generate a project from.
         api_key: API key to use for the Aineko Dream API.
         url: URL to use for the Aineko Dream API.
     """
-    try:
-        click.echo("Generating project from prompt...")
+    click.echo("Generating project from prompt...")
+    request_id = create_project_from_prompt(api_key, url, prompt)
+    click.echo(f"Successfully generated project with request ID: {request_id}.")
 
-        r = requests.get(
-            url=f"{url}/create/",
-            params={"prompt": prompt},
-            headers={"x-api-key": api_key},
-            timeout=300,
-        )
-        click.echo(f"Received response code: {r.status_code}")
-        if r.status_code == 200:
-            request_id = r.json()["request_id"]
-            click.echo(
-                "Successfully published project to: "
-                f"https://github.com/Convex-Labs/dream-catcher/tree/{request_id}. \n\n"  # pylint: disable=line-too-long
-                "To create a new project from this dream, run: \n\n"
-                f"aineko create --repo Convex-Labs/dream-catcher#{request_id}"
-            )
-        else:
-            click.echo(r.text)
-
-    except Exception as e:
-        raise click.ClickException(
-            f"Error connecting to Aineko Dream API at {url}: {e}"
-        )
+    status = check_request_status(api_key, url, request_id)
+    click.echo(status)
--- docs/aineko_dream.md
+++ docs/aineko_dream.md
@@ -7,7 +7,7 @@ Aineko Dream leverages the power of generative AI to create a starter Aineko pip
 To generate a project, invoke the Aineko Dream CLI with a prompt with:
 
 ```bash
-poetry run aineko dream "create a pipeline that scrapes twitter and analyses the results to identify trends" "API-KEY"
+poetry run aineko dream create --api-key API_KEY "create a pipeline that scrapes twitter and analyses the results to identify trends"
 ```
 
 replacing `API-KEY` with a valid Aineko Dream API key. Contact support@aineko.dev to get an API key to try this feature.
@@ -24,3 +24,10 @@ poetry run aineko create --repo Convex-Labs/dream-catcher#12345
 
 where the argument after `--repo` should be the unique ID associated to your generated project. This will be output in the result of the previous section.
 
+## Checking on the status of your Aineko Dream request
+
+After sending a request to the Aineko Dream CLI, you will be provided with a request ID that looks like `2c8341b7-9cb4-41f1-87ad-f363925fd2fa`. To check on the status of the request, use the following Aineko Dream CLI command:
+
+```bash
+poetry run aineko dream check 2c8341b7-9cb4-41f1-87ad-f363925fd2fa
+```
