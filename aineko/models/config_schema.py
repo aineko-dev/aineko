ff7a54a104688710baa2a97a95481afdd7aba2f3
Barabazs <31799121+Barabazs@users.noreply.github.com>
2024-02-20 15:10:56 +0100

    refactor: internal models (#162)
    * doc(config_schema): update Config docstring
    
    * feat(config_schema): introduce use of NodeSettings base model
    
    * doc: document the internal pydantic models and refer to them

--- aineko/models/config_schema.py
+++ aineko/models/config_schema.py
@@ -1,15 +1,38 @@
 # Copyright 2023 Aineko Authors
 # SPDX-License-Identifier: Apache-2.0
 """Internal models for pipeline config validation."""
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 from pydantic import BaseModel, Field
 
 from aineko.models.dataset_config_schema import DatasetConfig
 
 
+class NodeSettings(BaseModel):
+    """Node settings model.
+
+    This model is used to define settings for a node. The main use case is to
+    define the number of CPUs to use for a node. However, it can be extended to
+    include all Ray remote parameters as described in the [Ray documentation.](
+    https://docs.ray.io/en/latest/ray-core/api/doc/ray.remote.html#ray.remote/)
+    {:target="_blank"}
+    """
+
+    num_cpus: Optional[float] = Field(
+        None,
+        description="The number of CPUs to use for a node.",
+        gt=0.0,
+        examples=[1.0, 0.5],
+    )
+    model_config = {"extra": "allow"}
+
+
 class Config(BaseModel):
-    """Config model."""
+    """Pipeline configuration model.
+
+    Pipeline configurations are defined by the user in a YAML file. This model
+    is a representation of a serialized pipeline configuration.
+    """
 
     class Pipeline(BaseModel):
         """Pipeline model."""
@@ -19,12 +42,12 @@ class Config(BaseModel):
 
             class_name: str = Field(..., alias="class")
             node_params: Optional[dict] = None
-            node_settings: Optional[dict] = None
-            inputs: Optional[list] = None
-            outputs: Optional[list] = None
+            node_settings: Optional[NodeSettings] = None
+            inputs: Optional[List[str]] = None
+            outputs: Optional[List[str]] = None
 
         name: str
-        default_node_settings: Optional[dict] = None
+        default_node_settings: Optional[NodeSettings] = None
         nodes: Dict[str, Node]
         datasets: Dict[str, DatasetConfig]
 
---
+++ docs/api_reference/models.md
@@ -0,0 +1,27 @@
+# Internal Models
+
+This section describes the models used internally by Aineko to serialize and deserialize user configurations.
+
+## Pipeline Configuration
+
+::: aineko.models.config_schema.Config
+    options:
+        members: true
+        merge_init_into_class: false
+        show_bases: false
+
+## Node Configuration
+
+::: aineko.models.config_schema.NodeSettings
+    options:
+        members: true
+        merge_init_into_class: false
+        show_bases: false
+
+## Dataset Configuration
+
+::: aineko.models.dataset_config_schema.DatasetConfig
+    options:
+        members: true
+        merge_init_into_class: false
+        show_bases: false
